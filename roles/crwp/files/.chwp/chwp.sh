5b003da89ee13ba596c50c7783e26b9304383196
Eduard O <eduard.v@bk.ru>
2019-11-26 20:00:00 +0300

    several changes

--- roles/crwp/files/.chwp/chwp.sh
+++ roles/crwp/files/.chwp/chwp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+dev=$(ip r | grep def | awk -F ' ' '{ print $5}' | head -n1)
+getip=$(ip -f inet a show dev $dev | grep "inet" | awk -F' ' '{ print $2 }' | awk -F '/' '{ print $1 }' | head -n1)
+getmac=$(ip -f link a show dev $dev | grep "link/ether" | awk -F' ' '{ print $2 }')
+pcname=$(hostname)
+room="$(curl -G "http://192.168.5.186/pcs/?host=$pcname&ip=$getip&mac=$getmac" | awk -F 'Room: ' '{ print $2 }' | sed 's/<br>//')"
+if [ "$room" != ' ' ]; then
+    echo $room > /opt/crwp/.room
+fi
 export DISPLAY=:0
 /opt/crwp/crwp
 
